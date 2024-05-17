a4e1e9ab0d2d0aaffedf5f4aa2bb4e8902c0814d
Ethan Nguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2024-05-17 10:16:24 -0400

    Update modelKey.py
    Clarified return description and added a new line to separate the general description from the parameter descriptions.

--- Python/modelKey.py
+++ Python/modelKey.py
@@ -2,10 +2,11 @@ def create_key_entry(key_entry="<b>Key</b>",entry_color=color.black,list_num=0):
     """
     Helps create a basic model key in Glowscript.org. The model key will be
     located in the top left hand corner of the "Run this program" preview canvas.
+    
     :param key_entry: String name of the new entry to the key
     :param entry_color: Vector of the color in RGB format, vector(R,G,B)
     :param list_num: Integer placement of the key entry with 0 the title, 1 the second entry, etc.
-    :return:
+    :return: None
     """
 
     entry = label(pixel_pos=True, box=False, opacity=0, color=entry_color,
